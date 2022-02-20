f40ddf7bbfc887774caec038895735dd858b94b0
darkg <darkg@Darkghost>
2022-02-19 18:41:51 -0600

    Desarrollo de la cuarta clase

---
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -0,0 +1,31 @@
+
+package com.tienda.controller;
+import com.tienda.dao.ClienteDao;
+import com.tienda.model.Cliente;
+import java.util.Arrays;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+@Slf4j
+public class IndexController {
+  
+    @Autowired
+    private ClienteDao clienteDao;
+    
+    @GetMapping("/")
+    public String inicio(Model model){
+        log.info("Se muestra la página index de templates");
+        
+    
+        
+        var clientes=clienteDao.findAll();
+        model.addAttribute("clientes",clientes);
+        
+        return "index";
+    }
+}
